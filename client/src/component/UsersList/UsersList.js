8422dc8caac59d40bda06fcc44739ca36043fdc8
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-05-09 13:41:10 +0200

    component usersList created

---
+++ client/src/component/UsersList/UsersList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './userslist.css';
+
+function UsersList() {
+  return (
+    <ul>
+      <li>liste</li>
+    </ul>
+  );
+}
+
+UsersList.propTypes = {
+};
+UsersList.defaultProps = {
+};
+export default React.memo(UsersList);
